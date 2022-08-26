588702625d5f50485076422258e32d209aabcdc0
Alper <as.alper1@hotmail.com>
2022-08-26 11:13:42 +0300

    projs

---
+++ SQLBACKUP/SqlFuncs.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLBACKUP
+{
+    public class SqlFuncs
+    {
+        public SqlConnection SqlConnection()
+        {
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString =
+              "Data Source=" + "."/*conString.Text*/ + ";" + "Initial Catalog= master ;" + "User id=" + "sa"/*conUsername.Text*/ + ";" + "Password=" + "guncel@123"/*conPassword.Text*/ + ";";
+
+            return conn;
+        }
+
+        public SqlConnection SqlConnectionWindowsAuth()
+        {
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString =
+              "Data Source=" + "."/*conString.Text*/ + ";" + "Initial Catalog= master ;" + "User id=" + "sa"/*conUsername.Text*/ + ";" + "Password=" + "guncel@123"/*conPassword.Text*/ + ";";
+
+            return conn;
+        }
+    }
+}
